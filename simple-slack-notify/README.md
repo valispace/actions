7b2a9a2fdd03ae9733c23ad1381775b0e787550e
ricardo-dematos <108014795+ricardo-dematos@users.noreply.github.com>
2022-12-15 20:48:14 +0000

    Action 'simple-slack-notify' added

---
+++ simple-slack-notify/README.md
@@ -0,0 +1,56 @@
+# Valispace simple-slack-notify Action
+
+This is a fork from [adamkdean/simple-slack-notify](https://github.com/adamkdean/simple-slack-notify), _which is no longer maintained_, in order to update the action to Node16.
+
+## Usage
+
+<!-- start usage -->
+```yaml
+uses: valispace/actions/simple-slack-notify@master
+with:
+  # The channel you want to send to, such as '#general'.
+  # Required: false
+  # Default: ''
+  channel: '#general'
+  # Disable JS evaluation of strings.
+  # Required: false
+  # Default: ''
+  disable_eval: 'true'
+  # Used to override the default username.
+  # Required: false
+  # Default: ''
+  username: 'valispace'
+  # The color you want to use (can be 'good', 'danger', 'warning' or a hex code).
+  # Required: false
+  # Default: ''
+  color: 'good'
+  # Pass the job status through and omit color for status based color.
+  # Required: false
+  # Default: ''
+  status: 'success'
+  # The message that you want to send regardless of status.
+  # Required: false
+  # Default: ''
+  text: 'A message from GitHub Actions.'
+  # The message to send if status is 'success'.
+  # Required: false
+  # Default: ''
+  success_text: 'Success!'
+  # The message to send if status is 'failure'.
+  # Required: false
+  # Default: ''
+  failure_text: 'Failure!'
+  # The message to send if status is 'cancelled'.
+  # Required: false
+  # Default: ''
+  cancelled_text: 'Cancelled!'
+  # JSON string containing an array of fields to attach to the notification.
+  # Required: false
+  # Default: '[]'
+  fields: '[]'
+```
+<!-- end usage -->
+
+## License
+
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
---
+++ simple-slack-notify/src/index.js
@@ -0,0 +1,74 @@
+//         ___ _            _
+//        / __(_)_ __  _ __| |___
+//        \__ \ | '  \| '_ \ / -_)
+//    ___ |___/_|_|_|_| .__/_\___| _   _  __
+//   / __| |__ _ __| ||_|| \| |___| |_(_)/ _|_  _
+//   \__ \ / _` / _| / / | .` / _ \  _| |  _| || |
+//   |___/_\__,_\__|_\_\ |_|\_\___/\__|_|_|  \_, |
+//  Slack notification action that just works |__/
+// Copyright 2020 Adam K Dean <adamkdean@googlemail.com>
+
+const core = require('@actions/core')
+const { IncomingWebhook } = require('@slack/webhook')
+
+try {
+  if (!process.env.SLACK_WEBHOOK_URL) {
+    throw new Error('SLACK_WEBHOOK_URL is not set!')
+  }
+  const slack = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
+
+  /* eslint-disable no-eval */
+  const disableEval = !!core.getInput('disable_eval')
+  const env = process.env // eslint-disable-line
+  const envsubst = (str) => (disableEval ? str : eval(`\`${str}\``))
+
+  const channel = envsubst(core.getInput('channel'))
+  const username = envsubst(core.getInput('username'))
+  const status = envsubst(core.getInput('status'))
+  const successText = envsubst(core.getInput('success_text'))
+  const failureText = envsubst(core.getInput('failure_text'))
+  const cancelledText = envsubst(core.getInput('cancelled_text'))
+  const fields = JSON.parse(envsubst(core.getInput('fields')) || '[]')
+
+  let color = envsubst(core.getInput('color'))
+  let text = envsubst(core.getInput('text'))
+
+  // If color isn't set but status is, infer the color
+  if (!color) {
+    if (status === 'success') {
+      color = 'good'
+    } else if (status === 'failure') {
+      color = 'danger'
+    } else if (status === 'cancelled') {
+      color = 'warning'
+    }
+  }
+  // If text isn't set, check for status specific text
+  if (!text) {
+    if (status === 'success') {
+      text = successText || 'No success text specified.'
+    } else if (status === 'failure') {
+      text = failureText || 'No failure text specified.'
+    } else if (status === 'cancelled') {
+      text = cancelledText || 'No cancelled text specified.'
+    }
+  }
+
+  // Send the notification
+  ;(async () => {
+    await slack.send({
+      channel,
+      username,
+      attachments: [
+        {
+          fallback: text,
+          text,
+          color,
+          fields
+        }
+      ]
+    })
+  })()
+} catch (error) {
+  core.setFailed(error.message)
+}
