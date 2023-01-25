40678a28273a09f39ec76712de27e7ac02796961
ricardo-dematos <108014795+ricardo-dematos@users.noreply.github.com>
2023-01-25 18:46:26 +0000

    Action 'simple-slack-notify' updated

--- simple-slack-notify/src/index.js
+++ simple-slack-notify/src/index.js
@@ -13,17 +13,12 @@ const { IncomingWebhook } = require('@slack/webhook')
 
 try {
 
-  const webhook = envsubst(core.getInput('webhook'))
-  if (!webhook) {
-    throw new Error('webhook is not set!')
-  }
-  const slack = new IncomingWebhook(webhook)
-
   /* eslint-disable no-eval */
   const disableEval = !!core.getInput('disable_eval')
   const env = process.env // eslint-disable-line
   const envsubst = (str) => (disableEval ? str : eval(`\`${str}\``))
 
+  const webhook = envsubst(core.getInput('webhook'))
   const channel = envsubst(core.getInput('channel'))
   const username = envsubst(core.getInput('username'))
   const status = envsubst(core.getInput('status'))
@@ -55,6 +50,11 @@ try {
       text = cancelledText || 'No cancelled text specified.'
     }
   }
+
+  if (!webhook) {
+    throw new Error('webhook is not set!')
+  }
+  const slack = new IncomingWebhook(webhook)
 
   // Send the notification
   ;(async () => {
